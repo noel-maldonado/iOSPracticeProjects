9e5a7f4f83927b411fe8b6cc7e4ccd891d79afee
noel_maldonado <noel450@gmail.com>
2020-09-15 10:51:39 -0400

    new project

---
+++ Tip Calculator/Tip Calculator/CalculateViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Noel Maldonado on 9/15/20.
+//  Copyright © 2020 Noel Maldonado. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tip Calculator/Tip Calculator/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  Tip Calculator
+//
+//  Created by Noel Maldonado on 9/15/20.
+//  Copyright © 2020 Noel Maldonado. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
