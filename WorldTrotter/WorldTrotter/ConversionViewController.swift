f3cbb6b634d52fd0b1bce553467ee2b7764ef3e1
noel_maldonado <noel450@gmail.com>
2020-09-16 16:36:57 -0400

    update

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -13,21 +13,35 @@ class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrentheitValue = fahrenheitValue {
+            return fahrentheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("CoversionViewController loaded its view.")
+        
+        updateCelsiusLabel()
     }
     
+    
+    
     @IBAction func farenheitFieldEditingChanged(_ textField: UITextField) {
-//        celsiusLabel.text = textField.text
         
-        
-        //if textfield has text and text is not empty it will set text to celsiusLabel
-        //comma acts like an 'and'
-        if let text = textField.text, !text.isEmpty {
-            celsiusLabel.text = text
+
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
         } else {
-            celsiusLabel.text = "???"
+            fahrenheitValue = nil
         }
         
     }
@@ -35,6 +49,15 @@ class ConversionViewController: UIViewController {
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
         textField.resignFirstResponder()
     }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = "\(celsiusValue.value)"
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
     
 }
 
