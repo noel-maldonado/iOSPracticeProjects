f6165556cc0fcbb2f01430359a0493b72be603ab
noel_maldonado <noel450@gmail.com>
2020-09-18 22:52:15 -0400

    update

--- WorldTrotter/WorldTrotter/Controller/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/Controller/ConversionViewController.swift
@@ -13,6 +13,8 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
+    
+    
     var fahrenheitValue: Measurement<UnitTemperature>? {
         didSet {
             updateCelsiusLabel()
@@ -50,8 +52,8 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     @IBAction func farenheitFieldEditingChanged(_ textField: UITextField) {
         
 
-        if let text = textField.text, let value = Double(text) {
-            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        if let text = textField.text, let number = numberFormatter.number(from: text) {
+            fahrenheitValue = Measurement(value: number.doubleValue, unit: .fahrenheit)
         } else {
             fahrenheitValue = nil
         }
@@ -72,8 +74,11 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
-        let existingTextHasDecimalSeperator = textField.text?.range(of: ".")
-        let replacementTextHasDecimalSeperator = string.range(of: ".")
+        let currentLocale = Locale.current
+        let decimalSeparator = currentLocale.decimalSeparator ?? "."
+        
+        let existingTextHasDecimalSeperator = textField.text?.range(of: decimalSeparator)
+        let replacementTextHasDecimalSeperator = string.range(of: decimalSeparator)
         
         if existingTextHasDecimalSeperator != nil, replacementTextHasDecimalSeperator != nil {
                 return false
