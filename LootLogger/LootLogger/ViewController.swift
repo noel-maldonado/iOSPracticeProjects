b172131c335014be1b5d2d3084832256f8ab3492
noel_maldonado <noel450@gmail.com>
2020-09-28 20:11:47 -0400

    new project

---
+++ LootLogger/LootLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by Noel Maldonado on 9/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
