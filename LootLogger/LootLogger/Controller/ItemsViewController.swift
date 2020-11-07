7cbf54d69f1ba5d63ad906a8a666ef3bbb780481
noel_maldonado <noel450@gmail.com>
2020-11-06 23:14:43 -0500

    update

--- LootLogger/LootLogger/Controller/ItemsViewController.swift
+++ LootLogger/LootLogger/Controller/ItemsViewController.swift
@@ -23,9 +23,11 @@ class ItemsViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //Create an instance of UITableViewCell with default appreance
-        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+//        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        // get a new or recyled cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
         
-        // Set the Text on the cell with the description of the iterm
+        // Set the Text on the cell with the description of the item
         // that is at the nth index of items, where n = row this cell
         // will appear in on the table view
         let item = itemStore.allitems[indexPath.row]
